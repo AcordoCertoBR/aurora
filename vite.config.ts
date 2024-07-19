8649a2521d0ca50b59c85f4aadd3426249ec8307
gi-flor <giselea.flor@acordocerto.com.br>
2024-07-19 10:37:20 -0300

    feat: add colors tokens

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: [
+          `@import "src/core/tokens/colors.scss";`
+        ]
+      }
+    }
+  }
 })
