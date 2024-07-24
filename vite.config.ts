21dfb856aa0f938a8dedc62e94383ff3b30c872f
gi-flor <giselea.flor@acordocerto.com.br>
2024-07-24 15:38:41 -0300

    chore: add pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import dotenv from 'dotenv'
+import pkg from './package.json'
+import { resolve } from 'path'
+
+dotenv.config()
+
+process.env['VITE_LIB_VERSION'] = pkg.version
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts({ include: ['lib'] })],
+  build: {
+    copyPublicDir: false,
+    target: 'es2015',
+    lib: {
+      entry: resolve(__dirname, './lib/main.ts'),
+      name: 'aurora',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime'],
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
