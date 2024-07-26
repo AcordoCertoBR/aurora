c020760944748316c4b5a7f3e48f9bcf254b4748
Airton Vancin Junior <airtonvancin@gmail.com>
2024-07-26 16:34:11 -0300

    fix: build lib npm to publish

---
+++ lib/components/Novo/index.tsx
@@ -0,0 +1,5 @@
+import './styles.scss'
+
+export function Novo() {
+  return <div className="novo">Novo</div>
+}
--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,52 @@ import dts from 'vite-plugin-dts'
 import dotenv from 'dotenv'
 import pkg from './package.json'
 import { resolve } from 'path'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
+
+import fs from 'fs'
 
 dotenv.config()
 
 process.env['VITE_LIB_VERSION'] = pkg.version
 
-// https://vitejs.dev/config/
+function mapComponents(dir) {
+  const componentsDir = resolve(__dirname, dir)
+  const components = fs
+    .readdirSync(componentsDir)
+    .filter((file) => fs.statSync(resolve(componentsDir, file)).isDirectory())
+    .reduce((acc, folder) => {
+      acc[folder] = resolve(componentsDir, folder, 'index.tsx')
+      return acc
+    }, {})
+  return components
+}
+
 export default defineConfig({
-  plugins: [react(), dts({ include: ['lib'] })],
+  plugins: [react(), dts({ include: ['lib'] }), libInjectCss()],
   build: {
     copyPublicDir: false,
     target: 'es2015',
     lib: {
-      entry: resolve(__dirname, './lib/main.ts'),
+      entry: {
+        main: resolve(__dirname, 'lib/main.ts'),
+        ...mapComponents('lib/components'),
+      },
+      fileName: (format, entryName) =>
+        entryName === 'main'
+          ? `main.${format}.js`
+          : `components/${entryName}/index.${format}.js`,
       name: 'aurora',
-      fileName: 'aurora',
-      formats: ['es'],
+      formats: ['es', 'cjs'],
     },
+    cssCodeSplit: true,
     rollupOptions: {
       external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
+        assetFileNames: ({ name }) => {
+          const isCSS = name.endsWith('.css')
+          return isCSS && 'components/[name]/styles[extname]'
+        },
+
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
@@ -31,6 +57,9 @@ export default defineConfig({
         },
       },
     },
+    sourcemap: true,
+    minify: 'terser',
+    emptyOutDir: true,
   },
   css: {
     preprocessorOptions: {
