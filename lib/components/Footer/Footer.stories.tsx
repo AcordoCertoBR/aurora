651594afd26e07191a3d3d11166d9e657b104d6f
Vinícius Pacheco <viniciuuspacheco@gmail.com>
2024-08-08 18:37:15 -0300

    feat: add full footer

--- lib/components/Footer/Footer.stories.tsx
+++ lib/components/Footer/Footer.stories.tsx
@@ -1,7 +1,6 @@
 import { Meta, StoryObj } from '@storybook/react'
 import { LogoPrimaryCP } from '../Logo'
 import { Footer } from './index'
-import { Text } from '../Text'
 
 const meta: Meta<typeof Footer> = {
   title: 'Components/Footer',
@@ -16,20 +15,110 @@ type Story = StoryObj<typeof Footer>
 export const Default: Story = {
   args: {
     logo: <LogoPrimaryCP />,
-    notes: (
-      <Text as="h2" variant="body-medium" weight="regular">
-        As informações apresentadas nos relatórios de auto consulta, tais como:
-        score, negativação e verificação de consultas realizadas no CPF são
-        originárias do SCPC - banco de dados de proteção ao crédito administrado
-        pela BOA VISTA SERVIÇOS S.A. inscrita no CNPJ nº 11.725.176/0001-27 -,
-        empresa do Grupo Equifax.
-      </Text>
-    ),
+    links: [
+      {
+        category: 'Quem Somos',
+        items: [
+          {
+            name: 'Sobre o Consumidor Positivo',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Termos e Políticas',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Blog',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Evite fraudes',
+            url: 'https://google.com',
+          },
+        ],
+      },
+      {
+        category: 'Nossas soluções',
+        items: [
+          {
+            name: 'Cartões de Crédito',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Score',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Acordo Certo',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Cadastro Positivo',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Consultar CPF Grátis',
+            url: 'https://google.com',
+          },
+        ],
+      },
+      {
+        category: 'Blog',
+        items: [
+          {
+            name: 'Página Inicial',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Educação Financeira',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Score',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Cartão de Crédito',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Empréstimos',
+            url: 'https://google.com',
+          },
+        ],
+      },
+      {
+        category: 'Contato',
+        items: [
+          {
+            name: 'Fale Conosco',
+            url: 'https://google.com',
+          },
+          {
+            name: 'Central de Ajuda',
+            url: 'https://google.com',
+          },
+          {
+            name: '3003 - 0201 ( Das 8h às 20h )',
+          },
+          {
+            name: '0800 727 0201 ( Cadastro Positivo )',
+          },
+        ],
+      },
+    ],
+    socials: {
+      instagram: 'https://instagram.com',
+      facebook: 'https://facebook.com',
+      youtube: 'https://youtube.com',
+      linkedin: 'https://linkedin.com',
+    },
+    address:
+      'Rua Cardeal Arcoverde nº 2365, Conjuntos. 81 a 84, Sala 03, Bairro: Pinheiros, na cidade de São Paulo, Estado de São Paulo, CEP 05407-003',
+    notes:
+      'As informações apresentadas nos relatórios de auto consulta, tais como: score, negativação e verificação de consultas realizadas no CPF são originárias do SCPC - banco de dados de proteção ao crédito administrado pela BOA VISTA SERVIÇOS S.A. inscrita no CNPJ nº 11.725.176/0001-27 -, empresa do Grupo Equifax.',
     certificates: ['sslblindado', 'reclameaqui', 'pcidss'],
-    copyrights: (
-      <Text as="h2" variant="body-medium" weight="regular">
-        © 2024 Consumidor Positivo Ltda. - Todos os direitos reservados
-      </Text>
-    ),
+    copyrights:
+      '© 2024 Consumidor Positivo Ltda.- Todos os direitos reservados',
   },
 }
--- lib/components/Footer/index.tsx
+++ lib/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from 'react'
 import './styles.scss'
+import { Text } from '../Text'
 import fintech2022Logo from '../../assets/certifications/fintech-2022.svg'
 import fintech2023Logo from '../../assets/certifications/fintech-2023.svg'
 import pcidssLogo from '../../assets/certifications/pci-dss.svg'
@@ -8,6 +9,33 @@ import ra1000Logo from '../../assets/certifications/ra1000.svg'
 import reclameaquiLogo from '../../assets/certifications/reclame-aqui.svg'
 import scaleupLogo from '../../assets/certifications/scale-up.svg'
 import sslblindadoLogo from '../../assets/certifications/ssl-blindado.svg'
+
+import {
+  IconInstagram,
+  IconFacebook,
+  IconYoutube,
+  IconLinkedin,
+} from '../icons'
+import classNames from 'classnames'
+
+const socialsMap = {
+  instagram: {
+    name: 'Instagram',
+    icon: <IconInstagram />,
+  },
+  facebook: {
+    name: 'Facebook',
+    icon: <IconFacebook />,
+  },
+  youtube: {
+    name: 'Youtube',
+    icon: <IconYoutube />,
+  },
+  linkedin: {
+    name: 'Linkedin',
+    icon: <IconLinkedin />,
+  },
+}
 
 const certificatesMap = {
   fintech2022: {
@@ -46,7 +74,17 @@ const certificatesMap = {
 
 type FooterProps = {
   logo: ReactNode | string | JSX.Element | JSX.Element[]
-  links: [{ category: string; links: [{ name: string; url: string }] }]
+  links: Array<{
+    category: string
+    items: Array<{ name: string; url?: string }>
+  }>
+  socials: {
+    instagram?: string
+    facebook?: string
+    youtube?: string
+    linkedin?: string
+  }
+  address: string
   certificates: Array<keyof typeof certificatesMap>
   notes: ReactNode | string | JSX.Element | JSX.Element[]
   copyrights: ReactNode | string | JSX.Element | JSX.Element[]
@@ -54,6 +92,9 @@ type FooterProps = {
 
 export const Footer = ({
   logo,
+  links,
+  socials,
+  address,
   certificates,
   notes,
   copyrights,
@@ -61,19 +102,99 @@ export const Footer = ({
   const usedCertificates = certificates.map(
     (certificate) => certificatesMap[certificate],
   )
+
+  const usedSocials = Object.keys(socials).map((social) => {
+    return {
+      ...socialsMap[social as keyof typeof socialsMap],
+      url: socials[social as keyof typeof socials],
+    }
+  })
+
+  const handleClick = (url: string) => {
+    window.open(url, '_blank')
+  }
+
+  console.log(usedSocials)
+
   return (
     <footer className="au-footer">
       <section className="au-footer__logo">{logo}</section>
-      <section className="au-footer__links"></section>
+      <section className="au-footer__links">
+        {links.map(({ category, items }, index) => {
+          return (
+            <div key={index} className="au-footer__links__category">
+              <Text as="h2" variant="heading-micro" weight="bold">
+                {category}
+              </Text>
+              {items.map(({ name, url }, index) => {
+                return (
+                  <div
+                    className={classNames('au-footer__links', {
+                      clickable: !!url,
+                    })}
+                    key={index}
+                    onClick={() => url && handleClick(url)}>
+                    <Text
+                      as="a"
+                      variant="body-medium"
+                      weight="regular"
+                      color="secondary">
+                      {name}
+                    </Text>
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+        <div className="au-footer__links__category">
+          <Text as="h2" variant="heading-micro" weight="bold">
+            Siga a gente
+          </Text>
+          <div className="au-footer__links__socials">
+            {usedSocials.map(({ icon, url }, index) => {
+              return (
+                <div
+                  className={classNames('au-footer__links', {
+                    clickable: !!url,
+                  })}
+                  key={index}
+                  onClick={() => url && handleClick(url)}>
+                  {icon}
+                </div>
+              )
+            })}
+          </div>
+          <Text
+            as="h2"
+            variant="body-medium"
+            weight="regular"
+            color="secondary">
+            {address}
+          </Text>
+        </div>
+      </section>
       <section className="au-footer__certifications">
         <div className="au-footer__certifications__certificates">
           {usedCertificates.map(({ logo, name }, index) => {
             return <img key={index} src={logo} alt={name} />
           })}
         </div>
-        <div className="au-footer__certifications__notes">{notes}</div>
+        <div className="au-footer__certifications__notes">
+          <Text
+            as="h2"
+            variant="body-medium"
+            weight="regular"
+            color="secondary">
+            {notes}
+          </Text>
+        </div>
       </section>
-      <section className="au-footer__copyrights">{copyrights}</section>
+      <section className="au-footer__copyrights">
+        <Text as="h2" variant="body-medium" weight="regular" color="secondary">
+          {copyrights}
+        </Text>
+      </section>
     </footer>
   )
 }
