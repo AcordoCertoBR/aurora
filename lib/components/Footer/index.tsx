f66070807658ec13b1305270ab2e7796cb5ef02f
julianaesteves <juliana.trindade@bandtec.com.br>
2024-09-05 18:53:55 -0300

    refactor: remove notes, add appStore

--- lib/components/Footer/index.tsx
+++ lib/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 import { Text } from '../Text'
 import { FooterProps } from './types'
-import { certificatesMap, socialsMap } from './data'
+import { certificatesMap, socialsMap, storesMap } from './data'
 import classNames from 'classnames'
 import './styles.scss'
 
@@ -10,8 +10,8 @@ export const Footer = ({
   socialLinks,
   address,
   certificates,
-  notes,
   copyrights,
+  stores = {},
 }: FooterProps) => {
   const usedCertificates = certificates.map(
     (certificate) => certificatesMap[certificate],
@@ -21,6 +21,13 @@ export const Footer = ({
     return {
       ...socialsMap[social as keyof typeof socialsMap],
       url: socialLinks[social as keyof typeof socialLinks],
+    }
+  })
+
+  const usedStores = Object.keys(stores).map((store) => {
+    return {
+      ...storesMap[store as keyof typeof storesMap],
+      url: stores[store as keyof typeof stores],
     }
   })
 
@@ -86,30 +93,36 @@ export const Footer = ({
             </Text>
           </div>
         </div>
-        <div className="au-footer-full__certificates">
-          <div className="au-footer-full__certificates-logos">
+        <div className="au-footer-full__bottom">
+          <div className="au-footer-full__bottom-certificates">
             {usedCertificates.map(({ logo, name }, index) => {
               return <img key={index} src={logo} alt={name} />
             })}
           </div>
-          <div className="au-footer-full__certificates__notes">
+          <div className="au-footer-full__bottom-side">
+            <div className="au-footer-full__stores">
+              {usedStores.map(({ icon, url, name }, index) => {
+                return (
+                  <img
+                    className={classNames('au-footer-full__stores-logo', {
+                      'is-clickable': !!url,
+                    })}
+                    key={index}
+                    src={icon}
+                    alt={name}
+                    onClick={() => url && handleClick(url)}
+                  />
+                )
+              })}
+            </div>
             <Text
               as="h2"
               variant="body-medium"
               weight="regular"
               color="secondary">
-              {notes}
+              {copyrights}
             </Text>
           </div>
-        </div>
-        <div className="au-footer-full__copyrights">
-          <Text
-            as="h2"
-            variant="body-medium"
-            weight="regular"
-            color="secondary">
-            {copyrights}
-          </Text>
         </div>
       </footer>
     )
@@ -148,15 +161,6 @@ export const Footer = ({
             return <img key={index} src={logo} alt={name} />
           })}
         </div>
-        <div className="au-footer__content-notes">
-          <Text
-            as="h2"
-            variant="body-medium"
-            weight="regular"
-            color="secondary">
-            {notes}
-          </Text>
-        </div>
         <div className="au-footer__content-social">
           <Text as="h2" variant="heading-micro" weight="bold">
             Siga a gente
@@ -173,15 +177,6 @@ export const Footer = ({
         </div>
       </div>
       <div className="au-footer__bottom">
-        <div className="au-footer__bottom-notes">
-          <Text
-            as="h2"
-            variant="body-medium"
-            weight="regular"
-            color="secondary">
-            {notes}
-          </Text>
-        </div>
         <div className="au-footer__bottom-copyrights">
           <Text
             as="h2"
