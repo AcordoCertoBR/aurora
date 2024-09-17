9a9e16952fe654f4ec72305bd067e31f77b0e874
gi-flor <giselea.flor@acordocerto.com.br>
2024-09-17 15:22:52 -0300

    feat: add password field component

---
+++ lib/components/form/PasswordField/PasswordField.stories.tsx
@@ -0,0 +1,82 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { PasswordField } from './index'
+
+const meta: Meta<typeof PasswordField> = {
+  title: 'Components/form/PasswordField',
+  component: PasswordField,
+  tags: ['autodocs'],
+}
+
+export default meta
+
+type Story = StoryObj<typeof PasswordField>
+
+const commonArgs = {
+  label: 'Digite sua senha',
+  placeholder: 'Text',
+  style: { maxWidth: '272px' },
+}
+
+export const Default: Story = {
+  args: {
+    id: 'default-input',
+    ...commonArgs,
+  },
+}
+
+export const Focus: Story = {
+  args: {
+    id: 'focus-input',
+    autoFocus: true,
+    ...commonArgs
+  }
+}
+
+export const Required: Story = {
+  args: {
+    id: 'required-input',
+    requiredInput: true,
+    required: true,
+    ...commonArgs,
+  },
+}
+
+export const Optional: Story = {
+  args: {
+    id: 'optional-input',
+    optional: true,
+    ...commonArgs,
+  },
+}
+
+export const Success: Story = {
+  args: {
+    id: 'success-input',
+    success: true,
+    ...commonArgs,
+  },
+}
+
+export const Error: Story = {
+  args: {
+    id: 'error-input',
+    error: true,
+    errorMessage: 'Senha incorreta',
+    ...commonArgs,
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    id: 'disabled-input',
+    disabled: true,
+    ...commonArgs,
+  },
+}
+
+export const WithoutLabel: Story = {
+  args: {
+    style: commonArgs.style,
+    placeholder: commonArgs.placeholder
+  },
+}
