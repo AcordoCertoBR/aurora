cceea580385781a0933e53870386ab75ccb12fd9
gi-flor <giselea.flor@acordocerto.com.br>
2024-09-24 18:16:12 -0300

    feat: checked style

--- lib/components/form/CheckboxField/index.tsx
+++ lib/components/form/CheckboxField/index.tsx
@@ -2,6 +2,7 @@ import classNames from 'classnames'
 import Field from '../Field'
 
 import './styles.scss'
+import { IconCheck } from '../../icons'
 
 type CheckboxFieldProps = React.InputHTMLAttributes<HTMLInputElement> & {
   label: string
@@ -14,18 +15,30 @@ export const CheckboxField = ({
   error,
   errorMessage,
   disabled,
+  id,
+  ...props
 }: CheckboxFieldProps) => {
   const checkboxClasses = classNames('au-checkbox', {
     'au-checkbox--error': !!error,
     'au-checkbox--disabled': !!disabled,
   })
-  
+
   return (
     <div className={checkboxClasses}>
-      <div>
-        <input type="checkbox" />
-        <label>{label}</label>
-      </div>
+      <label htmlFor={id} className="au-checkbox__holder">
+        <input
+          className="au-checkbox__input"
+          type="checkbox"
+          id={id}
+          {...props}
+        />
+        <span className="au-checkbox__check">
+          <IconCheck rawColor="#fff" />
+        </span>
+        <span className="au-checkbox__label">
+          {label}
+        </span>
+      </label>
       <Field.ErrorMessage hasError={!!error} message={errorMessage} />
     </div>
   )
