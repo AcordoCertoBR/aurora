245334c6ededdfe0c4e6919dc66bf88b3c49bdc2
Mateus Miguel <webmateusmiguel@gmail.com>
2024-10-08 18:21:46 -0300

    feat: textarea

---
+++ lib/components/form/TextareaField/TexareaField.stories.tsx
@@ -0,0 +1,96 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { TextAreaField } from './index'
+
+const meta: Meta<typeof TextAreaField> = {
+  title: 'Components/form/TextAreaField',
+  component: TextAreaField,
+  tags: ['autodocs'],
+  parameters: {
+    backgrounds: {
+      default: 'default',
+      values: [{ name: 'default', value: '#f1f1f1' }],
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof TextAreaField>
+
+const commonArgs = {
+  label: 'Label',
+  placeholder: 'Escreva aqui...',
+  style: { maxWidth: '272px' },
+}
+
+export const Default: Story = {
+  args: {
+    id: 'default-textarea',
+    rows: 4,
+    ...commonArgs,
+  },
+}
+
+export const CharCount: Story = {
+  args: {
+    id: 'charcount-textarea',
+    maxLength: 100,
+    ...commonArgs,
+  },
+}
+
+export const Required: Story = {
+  args: {
+    id: 'required-textarea',
+    required: true,
+    ...commonArgs,
+  },
+}
+
+export const Optional: Story = {
+  args: {
+    id: 'optional-textarea',
+    optional: true,
+    ...commonArgs,
+  },
+}
+
+export const Success: Story = {
+  args: {
+    id: 'success-textarea',
+    success: true,
+    ...commonArgs,
+  },
+}
+
+export const Error: Story = {
+  args: {
+    id: 'error-textarea',
+    error: true,
+    errorMessage: 'Mensagem de erro',
+    ...commonArgs,
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    id: 'disabled-textarea',
+    disabled: true,
+    ...commonArgs,
+  },
+}
+
+export const CustomRows: Story = {
+  args: {
+    id: 'customrows-textarea',
+    rows: 6,
+    ...commonArgs,
+  },
+}
+
+export const WithoutLabel: Story = {
+  args: {
+    style: commonArgs.style,
+    placeholder: commonArgs.placeholder,
+  },
+}
