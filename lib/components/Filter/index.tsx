3b6ebde79f81ba6bf04982c1ff3e7aab83f79ffa
Teresa Lazarini <teresalazarini@gmail.com>
2025-01-15 17:25:36 -0300

    feat: add clicked button

--- lib/components/Filter/index.tsx
+++ lib/components/Filter/index.tsx
@@ -1,19 +1,32 @@
 import { Button } from '@components/Button'
+import { useState } from 'react'
+import classNames from 'classnames'
 import './styles.scss'
 
 export type FilterProps = {
   content: { title: string;  onClick: () => void; type?: 'primary' | 'outlined' | 'ghost' } []
 }
 
+
+
 export const Filter= ({
   content
 }: FilterProps) => {
 
+const [isClicked, setIsClicked] = useState(false);
+  const buttonClass = classNames('au-filter__btn', {
+      'btn-clicked': isClicked
+    })
+
   return (
     <div className="au-filter">
       {content?.map((item) => {
+        const handleClick = () => {
+          setIsClicked(!isClicked);
+          item.onClick()
+        };
           return (
-            <Button type={item.type ?? 'primary'} onClick={item.onClick}>
+            <Button className={buttonClass} type={item.type ?? 'primary'} onClick={handleClick} key={`item-${item}`}>
               {item.title}
             </Button>
           )
