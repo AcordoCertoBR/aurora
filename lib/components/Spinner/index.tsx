ef03b40b4fe58d3f0bab453701e256c6e4fd6d99
julianaesteves <juliana.trindade@bandtec.com.br>
2025-01-17 16:11:33 -0300

    fix: adjust gradient

--- lib/components/Spinner/index.tsx
+++ lib/components/Spinner/index.tsx
@@ -1,4 +1,3 @@
-import { COLOR_BRAND_BLUE_40 } from '@core/tokens'
 import './styles.scss'
 
 type SpinnerProps = {
@@ -6,10 +5,7 @@ type SpinnerProps = {
   color: string
 }
 
-export const Spinner: React.FC<SpinnerProps> = ({
-  size = 32,
-  color = COLOR_BRAND_BLUE_40,
-}) => {
+export const Spinner: React.FC<SpinnerProps> = ({ size, color }) => {
   return (
     <div className="spinner">
       <svg
@@ -22,18 +18,19 @@ export const Spinner: React.FC<SpinnerProps> = ({
           <linearGradient
             id={`ac-loading-${color}`}
             gradientUnits="objectBoundingBox"
-            x1="9%"
+            x1="10%"
             y1="0%"
-            x2="100%"
+            x2="90%"
             y2="10%">
             <stop offset="0" stop-color={color} stop-opacity="1" />
             <stop offset="1" stop-color={color} stop-opacity="0" />
           </linearGradient>
         </defs>
         <g stroke-width="7" stroke-linecap="round" fill="none">
-          <path stroke={color} d="M4,32 c0,15,12,28,28,28c8,0,16-4,21-9" />
+          <path stroke={color} d="M60,32 A28,28 0 1 1 4,32" />
+
           <path
-            d="M60,32 C60,16,47.111,4,32,4S4,16,4,32"
+            d="M60,32 A28,28.5 0 1 0 4,32"
             stroke={`url(#ac-loading-${color})`}
           />
         </g>
