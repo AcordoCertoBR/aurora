cbe001abe9a7fb46842b7d33d3d36db2c3232f50
Tiago Vaz <tiago.vaz@hotmail.com>
2025-03-14 10:41:41 -0300

    fix: export tabItem type and add "au-" prefix on tab main class

--- lib/components/Tabs/index.tsx
+++ lib/components/Tabs/index.tsx
@@ -1,66 +1,85 @@
-import classNames from 'classnames';
-import { Conditional } from '@components/misc';
-import { Button } from '@components/Button';
-import { useState } from 'react';
-import './styles.scss';
+import classNames from 'classnames'
+import { Conditional } from '@components/misc'
+import { Button } from '@components/Button'
+import { useState } from 'react'
+import './styles.scss'
 
 export interface TabsProps {
-  tabs: TabProps[];
-  areTabsHidden?: boolean;
-  initialTab?: string;
-  onClick: (value: string) => void;
+  tabs: TabItem[]
+  areTabsHidden?: boolean
+  initialTab?: string
+  onClick: (value: string) => void
 }
 
-interface TabProps {
-  tab: string;
-  title: string;
-  children?: React.ReactElement;
+export interface TabItem {
+  tab: string
+  title: string
+  children?: React.ReactElement
 }
 
-export const Tabs = ({ tabs, initialTab, onClick, areTabsHidden }: TabsProps) => {
-
-  const [isClicked, setIsClicked] = useState(false);
+export const Tabs = ({
+  tabs,
+  initialTab,
+  onClick,
+  areTabsHidden,
+}: TabsProps) => {
+  const [isClicked, setIsClicked] = useState(false)
   const [currButton, setCurrButton] = useState(initialTab ?? '')
   const [activeTab, setActiveTab] = useState(initialTab)
 
-  const handleClick = (item: TabProps) => {
+  const handleClick = (item: TabItem) => {
     onClick(item.tab)
     setCurrButton(item.tab)
     setActiveTab(item.tab)
-    setIsClicked(true);
-  };
+    setIsClicked(true)
+  }
 
-  const buttonClass = (item: TabProps) => {
-    const isActive = isClicked && item.tab === currButton || activeTab === item.tab
+  const buttonClass = (item: TabItem) => {
+    const isActive =
+      (isClicked && item.tab === currButton) || activeTab === item.tab
     return classNames('au-tabs__button', {
-      'active': isActive
-    });
-  };
+      active: isActive,
+    })
+  }
 
   return (
     <>
-      <Conditional condition={!areTabsHidden} renderIf={<div className="tabs">
-        <div className="au-tabs__container">
-          {tabs.map((item: TabProps) => {
-            return (
-              <Button key={`au-tabs-${item.tab}`}  className={buttonClass(item)}
-               type={'outlined'} onClick={() => handleClick(item)}>
-                {item.title}
-              </Button>
-            );
-          })}
-        </div>
-      </div>} renderElse={null}/>
-
-      {tabs.map(({ children, tab }: TabProps) => {
-        return (
-          <div className={`au-tabs__children children-${tab}`} key={`au-tabs-${tab}`}>
-            <Conditional condition={currButton === tab || activeTab === tab} renderIf={children} />
+      <Conditional
+        condition={!areTabsHidden}
+        renderIf={
+          <div className="au-tabs">
+            <div className="au-tabs__container">
+              {tabs.map((item: TabItem) => {
+                return (
+                  <Button
+                    key={`au-tabs-${item.tab}`}
+                    className={buttonClass(item)}
+                    type={'outlined'}
+                    onClick={() => handleClick(item)}>
+                    {item.title}
+                  </Button>
+                )
+              })}
+            </div>
           </div>
-        );
+        }
+        renderElse={null}
+      />
+
+      {tabs.map(({ children, tab }: TabItem) => {
+        return (
+          <div
+            className={`au-tabs__children children-${tab}`}
+            key={`au-tabs-${tab}`}>
+            <Conditional
+              condition={currButton === tab || activeTab === tab}
+              renderIf={children}
+            />
+          </div>
+        )
       })}
     </>
-  );
-};
+  )
+}
 
-export default Tabs;
+export default Tabs
