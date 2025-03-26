1cd2accd08874c05e975ee19bd5dc643a2b9db66
Kauan Batista <kauan.batista@acordocerto.com.br>
2025-03-26 13:28:55 -0300

    feat: EmailField component

---
+++ lib/components/form/EmailField/hook.ts
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+
+const emailDomains = [
+  'gmail.com',
+  'hotmail.com',
+  'yahoo.com',
+  'outlook.com',
+  'yahoo.com.br',
+  'uol.com.br',
+  'ig.com.br',
+  'terra.com.br',
+  'aol.com',
+  'live.com',
+  'msn.com',
+]
+
+export const useEmailAutocomplete = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [suggestions, setSuggestions] = useState<string[]>([])
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value
+    if (inputValue === value) return
+    setInputValue(value)
+
+    const atIndex = value.indexOf('@')
+    if (atIndex > -1) {
+      const domainPart = value.slice(atIndex + 1)
+      const filteredDomains = emailDomains.filter((domain) =>
+        domain.startsWith(domainPart),
+      )
+      setSuggestions(filteredDomains)
+      setIsDropdownOpen(filteredDomains.length > 0)
+    } else {
+      setSuggestions([])
+      setIsDropdownOpen(false)
+    }
+  }
+
+  const handleSuggestionClick = (suggestion: string) => {
+    const atIndex = inputValue.indexOf('@')
+    if (atIndex > -1) {
+      setInputValue(inputValue.slice(0, atIndex + 1) + suggestion)
+    }
+    setSuggestions([])
+    setIsDropdownOpen(false)
+  }
+
+  return {
+    inputValue,
+    setInputValue,
+    suggestions,
+    isDropdownOpen,
+    handleChange,
+    handleSuggestionClick,
+  }
+}
