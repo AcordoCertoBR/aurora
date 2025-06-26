31bac1e6beb81a198aa58d48b7f40e2a7c81e0c6
Tim Fontes <italo5123@hotmail.com>
2025-06-26 18:41:06 -0300

    feat: :sparkles: added badge expand property

--- lib/components/Header/parts/HeaderBadges.tsx
+++ lib/components/Header/parts/HeaderBadges.tsx
@@ -1,9 +1,17 @@
+import classNames from 'classnames'
 import { ReactNode } from 'react'
 
 export type HeaderBadgesProps = {
   children: ReactNode | string | JSX.Element | JSX.Element[]
+  limitBadgeWidth?: boolean
 }
 
-export const HeaderBadges = ({ children }: HeaderBadgesProps) => {
-  return <div className="au-header__badges">{children}</div>
+export const HeaderBadges = ({
+  children,
+  limitBadgeWidth = true,
+}: HeaderBadgesProps) => {
+  const componentClass = classNames('au-header__badges', {
+    'au-header__badges--limited-width': limitBadgeWidth,
+  })
+  return <div className={componentClass}>{children}</div>
 }
