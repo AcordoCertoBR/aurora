81a0006aba35fbfb8d6d19548dd08fd94f3cc924
gi-flor <gisele.araujo.dev@gmail.com>
2025-10-13 18:05:58 -0300

    test: add tag test

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vitest/config'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '@components', replacement: resolve(__dirname, 'lib/components') },
+      { find: '@core', replacement: resolve(__dirname, 'lib/core') },
+      { find: '@assets', replacement: resolve(__dirname, 'lib/assets') },
+    ],
+  },
   test: {
     environment: 'jsdom',
     globals: true,
