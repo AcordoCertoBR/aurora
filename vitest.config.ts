a0dbc44aaad5d88b89965f4f1bebbc3d8b5eb55a
gi-flor <gisele.araujo.dev@gmail.com>
2025-10-16 16:37:48 -0300

    chore: exclude images to test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -8,7 +8,10 @@ const __dirname = dirname(__filename)
 export default defineConfig({
   resolve: {
     alias: [
-      { find: '@components', replacement: resolve(__dirname, 'lib/components') },
+      {
+        find: '@components',
+        replacement: resolve(__dirname, 'lib/components'),
+      },
       { find: '@core', replacement: resolve(__dirname, 'lib/core') },
       { find: '@assets', replacement: resolve(__dirname, 'lib/assets') },
     ],
@@ -21,7 +24,15 @@ export default defineConfig({
       'lib/components/**/*.test.{ts,tsx}',
       'lib/components/**/*.{test,spec}.{ts,tsx}',
     ],
-    exclude: ['**/*.stories.*', '**/*.stories.@(ts|tsx|js|jsx|mdx)', 'dist/**'],
+    exclude: [
+      '**/*.stories.*',
+      '**/*.stories.@(ts|tsx|js|jsx|mdx)',
+      'dist/**',
+      'lib/components/Logo/ac/**',
+      'lib/components/Logo/cp/**',
+      'lib/components/icons/default/**',
+      'lib/components/icons/social/**',
+    ],
     coverage: {
       reporter: ['text', 'lcov', 'html'],
       include: ['lib/components/**/*.ts', 'lib/components/**/*.tsx'],
@@ -29,6 +40,10 @@ export default defineConfig({
         '**/*.stories.*',
         '**/*.stories.@(ts|tsx|js|jsx|mdx)',
         'dist/**',
+        'lib/components/Logo/ac/**',
+        'lib/components/Logo/cp/**',
+        'lib/components/icons/default/**',
+        'lib/components/icons/social/**',
       ],
     },
   },
